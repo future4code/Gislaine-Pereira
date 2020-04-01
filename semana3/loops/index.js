8e85584db4f1d08cf817aacc8730b370991cab48
Gislaine Costa <gislaine_costa@hotmail.com>
2020-04-01 17:32:17 -0300

    Reentrega - Resposta do desafio nª 3

---
+++ semana3/loops/index.js
@@ -0,0 +1,22 @@
+num = Math.floor(Math.random() * 100)
+
+console.log("Vamos jogar!")
+
+chute = Number(prompt("Chute um número entre 0 e 100"))
+
+let tentativas = 1
+
+while (num != chute) {
+  if (num < chute) {
+    chute = Number(prompt("O número chutado foi " + chute + "\n"  + "Errou feio, errou rude. É menor" + "\n" + "Tente outra vez"))
+    
+  }
+
+  else{
+    chute = Number(prompt("O número chutado foi " + chute + "\n" + "Errou feio, errou rude. É maior" + "\n" + "Tente outra vez"))
+  }
+
+  tentativas ++
+}
+
+console.log ("Acertô Miserávi!" + "\n" + "O número de tentativas foi: " + tentativas) 
