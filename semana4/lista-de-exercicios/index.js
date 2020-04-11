2d26765740bda078123ab3ad295f6c310232195c
Gislaine Costa <gislaine_costa@hotmail.com>
2020-04-11 01:52:28 -0300

    Exercício 2 - Leitura de código

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -2,4 +2,13 @@
 
 EXERCÍCIO 1 
     O valor impresso depende do valor indicado pelo usuário no prompt. Esta função vai multiplicar o valor 
-    de 100 pelo número indicado pelo usuário.*/
+    de 100 pelo número indicado pelo usuário.
+
+EXERCÍCIO 2 
+    Esta função leva em consideração 2 parâmetros: O tipo de investimento e o valor investido. 
+    Por exemplo, se o usuário escolher a Poupança, o valor investido será multiplicado por 1.03, 
+    se for Clipboard, 1.06 e assim por diante.  Ao final ele pede para imprimir no cosole qual valor
+    novoMontante = 165
+    segundoMontate = (vai ter um alert informando que o tipo de investimento é incorreto)
+
+    */
