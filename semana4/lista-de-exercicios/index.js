a2a66f1977ec3c94363f8a00301c31bf173adc85
Gislaine Costa <gislaine_costa@hotmail.com>
2020-04-11 01:44:52 -0300

    Exercício 1 - Leitura de código

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,5 @@
+/* ==========================  EXERCÍCIOS DE LEITURA DE CÓDIGO =====================================
+
+EXERCÍCIO 1 
+    O valor impresso depende do valor indicado pelo usuário no prompt. Esta função vai multiplicar o valor 
+    de 100 pelo número indicado pelo usuário.*/
