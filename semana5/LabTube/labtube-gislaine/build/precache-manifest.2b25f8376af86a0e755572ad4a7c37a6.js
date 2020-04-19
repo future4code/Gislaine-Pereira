b1bc2107efc8e5bac3b0198e43996c66c695404c
Gislaine Costa <gislaine_costa@hotmail.com>
2020-04-18 23:40:21 -0300

    Criação do Insta4

---
+++ semana5/LabTube/labtube-gislaine/build/precache-manifest.2b25f8376af86a0e755572ad4a7c37a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abcfdc4622377cfe315bd24d6241d938",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02d213f11d77f4c1da16",
+    "url": "/static/css/main.f6026a36.chunk.css"
+  },
+  {
+    "revision": "f71f9933202e44e3671a",
+    "url": "/static/js/2.ccf21209.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ccf21209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02d213f11d77f4c1da16",
+    "url": "/static/js/main.1254a7d0.chunk.js"
+  },
+  {
+    "revision": "02e41cc0d16be7d7c287",
+    "url": "/static/js/runtime-main.b5cd1974.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
