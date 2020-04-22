16459fc9f76cf682ca5b7fb99c86407a164f1920
Gislaine Costa <gislaine_costa@hotmail.com>
2020-04-22 20:56:02 -0300

    Deploy do exercício

---
+++ semana6/boiler-plate/build/precache-manifest.d23265da2896ab9ed4541cddcbb8acb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c750842f8721264027c022ee083bda8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13e19d7dae0480ff854e",
+    "url": "/static/css/main.66d6f871.chunk.css"
+  },
+  {
+    "revision": "078d8750ac3017505296",
+    "url": "/static/js/2.29e9a946.chunk.js"
+  },
+  {
+    "revision": "13e19d7dae0480ff854e",
+    "url": "/static/js/main.52343bbb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
