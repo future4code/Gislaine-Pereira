0da9f669d6b2779ab906bb29056cdd0d21fdf43e
Gislaine Costa <gislaine_costa@hotmail.com>
2020-04-22 16:52:36 -0300

    Início das atividades da semana6-Aula22

---
+++ semana6/labform/build/precache-manifest.d2e9c73bc0088203f1a106a5b3dc82de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47807050e9e7743c6f0a58e5509f4bb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4a332678fb6c0a46eb2",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b8a94d355ae2f7ca25ec",
+    "url": "/static/js/2.f40888b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f40888b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4a332678fb6c0a46eb2",
+    "url": "/static/js/main.e020be43.chunk.js"
+  },
+  {
+    "revision": "e1a9554b834f7101654e",
+    "url": "/static/js/runtime-main.9990d50f.js"
+  }
+]);
