ab9376f6b4e77678f5027f91b0647c3f32419d2b
Gislaine Costa Pereira <62241763+gislainecosta@users.noreply.github.com>
2020-05-01 03:57:04 -0300

    Termino dos exercícios

--- semana7/Aula27/atividades-gislaine/src/App.js
+++ semana7/Aula27/atividades-gislaine/src/App.js
@@ -12,27 +12,39 @@ const Aplicativo = styled.div`
 `
 const Opcoes = styled.section`
   display: flex;
-  padding: 5vw;
+  padding: 5vw 5vw 2vw 5vw;
   justify-content: space-between;
 `
 const TituloGeral = styled.h1`
   margin-bottom: 3vw;
 `
-const Secao = styled.section`
-`
-const Subtitulo = styled.h3`
+const ListaAtividades = styled.section `
+  text-align: start;
+  padding: 0vw 5vw;
 `
 const Selecione = styled.select`
   border: 1px solid black;
 `
 const Rotulo = styled.label`
-  font-size: 1em;
+  font-size: 1.5vw;
+  font-weight: bold;
 `
-const Botao = styled.button`
-  border: 1px solid black;
+const Paragrafo = styled.p`
+  font-size: 1.3vw;
   margin: 2vw;
-  height: 2.5vw;
-  padding: 0 0.5vw;
+`
+const Input = styled.input`
+  border: 1px solid black;
+  width: 3vw;
+  height:1.5vw;
+  padding: 0.2vw;
+`
+
+const Botao = styled.button`
+  background-color: black;
+  color: white;
+  width: 5vw;
+  height: 3vw;
 `
 
 
@@ -61,52 +73,97 @@ class App extends React.Component {
       "music", 
       "busywork"
     ],
-
-    isAleatorio: undefined
+    atividade:{},
+    numeroParticipantes: 0,
+    tipoAtividade: "",
+    linguagemNavegador: '',
   }
   
-  escolherAleatorio = () =>{
-    this.setState({ isAleatorio: true})
+
+  
+  numParticipantes = (event) =>{
+    //const numeroParticipantes = event.target.value;
+    this.setState({numeroParticipantes: event.target.value})
   }
 
-  escolherOpcoes = () => {
-    this.setState({ isAleatorio: false })
-  }
-  
   tipoAtividade = (event) =>{
-    const qualAtividade = event.target.value
-    console.log(qualAtividade)
+    //const tipoAtividade = event.target.value;
+   this.setState({tipoAtividade: event.target.value})
+  }  
+  
+  buscarAtividade = () =>{
+    if(this.state.tipoAtividade === 'aleatorio'){
+      axios
+      .get("https://www.boredapi.com/api/activity/")
+      .then(response => {
+        this.setState({ atividade: response.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    }else{
+      axios
+      .get(`https://www.boredapi.com/api/activity?type=${this.state.tipoAtividade}&participants=${this.state.numeroParticipantes}`)
+      .then(response => {
+        this.setState({ atividade: response.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+    }
   }
+  
 
   render (){
-
+    
     return (
       <Aplicativo>
         
-        <TituloGeral> Você está entediado? Escolha uma atvidade 🤹</TituloGeral>
-        <Secao>
-          <Subtitulo>Você quer escolher algum tipo?</Subtitulo><br/>
-          <Botao onClick={this.escolherAleatorio}>Aleatório, gosto de viver perigosamente</Botao>
-          <Botao onClick={this.escolherOpcoes}>Quero escolher alguma coisa, não confio no destino</Botao>
-        </Secao>
+        <TituloGeral> Você está entediado? Escolha uma atvidade 🤹</TituloGeral>        
         
-        <Secao>
-          <Opcoes>
-            <Rotulo for='atividades'> Selecione um tipo de Atividade: &nbsp;
-              <Selecione id='atividades' onChange={this.tipoAtividade}>
-                <option value={""}>Nenuma</option>
-                <option value={'aleatorio'}>Aleatório</option>
-                {this.state.tiposAtividades.map(atividade => {
-                  return (
-                    <option key={atividade} value={atividade}>
-                      {atividade}
-                    </option>
-                  );
-                })}
-              </Selecione>
-            </Rotulo>
-          </Opcoes>
-        </Secao>
+        <Opcoes>
+          <Rotulo for='atividades'> Selecione um tipo de atividade: &nbsp;
+            <Selecione id='atividades' onChange={this.tipoAtividade}>
+              <option value={'aleatorio'}>Aleatório</option>
+              {this.state.tiposAtividades.map(atividade => {
+                return (
+                  <option key={atividade} value={atividade}>
+                    {atividade}
+                  </option>
+                );
+              })}
+            </Selecione>
+          </Rotulo>
+          <Rotulo for="quantidade"> Quantos Participantes ? (Escolha de 1 à 5): &nbsp;
+            <Input
+              type = "number"
+              id = "quantidade"
+              onChange = {this.numParticipantes}
+              min = "1"
+              max = "5"
+              OnChange={this.numParticipantes} />
+          </Rotulo>
+          <Botao onClick={this.buscarAtividade}>Buscar</Botao>
+        </Opcoes>
+
+        <ListaAtividades>
+          <Paragrafo> <b>Atividade: &nbsp;</b>
+            {this.state.atividade.activity}
+          </Paragrafo>
+          <Paragrafo> <b>Acessibilidade: &nbsp;</b>
+            {this.state.atividade.accessibility}
+          </Paragrafo>
+          <Paragrafo> <b>Tipo: &nbsp;</b>
+            {this.state.atividade.type}
+          </Paragrafo>
+          <Paragrafo> <b> Participantes necessários: &nbsp;</b>
+            {this.state.atividade.participants}
+          </Paragrafo>
+          <Paragrafo> <b>Preço: &nbsp;</b>
+            {this.state.atividade.price}
+          </Paragrafo>
+        </ListaAtividades>
       
       </Aplicativo>
     );
