e1985f321c103195a17bed88640299dcb9c1813c
Gislaine Costa Pereira <62241763+gislainecosta@users.noreply.github.com>
2020-05-18 17:05:06 -0300

    Criação da aplicação e layout

---
+++ semana9/Astromatch/astromatch-gislaine/build/precache-manifest.7264a4c7b8f6d40573e74262bb3ceb96.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51d5b2a6f4ecabb55e2030c23a4a1ba5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "119379b4d052a8f2bed8",
+    "url": "/static/css/main.3b25a4f7.chunk.css"
+  },
+  {
+    "revision": "1e6ee78d99527c8960e2",
+    "url": "/static/js/2.065c89ed.chunk.js"
+  },
+  {
+    "revision": "3376583730ca32947c68cf0fdbcbb344",
+    "url": "/static/js/2.065c89ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "119379b4d052a8f2bed8",
+    "url": "/static/js/main.a00f83e0.chunk.js"
+  },
+  {
+    "revision": "2d6aaca4880b38941c2f",
+    "url": "/static/js/runtime-main.9844d08a.js"
+  },
+  {
+    "revision": "96cb615a29e9056a19f3901d0dee7348",
+    "url": "/static/media/Coracao.96cb615a.gif"
+  },
+  {
+    "revision": "c6516da309a77b198b967620aed78469",
+    "url": "/static/media/core.c6516da3.png"
+  },
+  {
+    "revision": "a4a9eaf7820f1ac62e2396573b9f3b3c",
+    "url": "/static/media/logo.a4a9eaf7.png"
+  }
+]);
