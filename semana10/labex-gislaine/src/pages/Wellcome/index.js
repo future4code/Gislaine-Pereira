ae11f8dd4f2c18afea535ddfb0a4517b993c0ae1
Gislaine Costa Pereira <62241763+gislainecosta@users.noreply.github.com>
2020-05-19 17:52:41 -0300

    Começo da estilização

--- semana10/labex-gislaine/src/pages/Wellcome/index.js
+++ semana10/labex-gislaine/src/pages/Wellcome/index.js
@@ -1,5 +1,18 @@
 import React, { useState } from "react";
+import './index.css';
+import styled from 'styled-components'
 import { useHistory } from "react-router-dom";
+import ImagemFundo from "../../img/fundo.jpg"
+import Logo from "../../img/Logo.png"
+
+const TelaToda = styled.div`
+   background-image: url("${ImagemFundo}");
+  background-color: #0d0d2b;
+  height: 100vh;
+  img {
+    width: 7vw;
+  }
+`
 
 const Wellcome = () => {
   const history = useHistory();
@@ -13,11 +26,11 @@ const Wellcome = () => {
   };
 
   return (
-    <div>
+    <TelaToda id="pagina-toda">
       <p>Home Usuário</p>
       <button onClick={goToAdminPage}>Administrador</button>
       <button onClick={goToUserPage}>Usuario</button>
-    </div>
+    </TelaToda>
   );
 };
 
