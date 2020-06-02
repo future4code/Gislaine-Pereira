6aaba58013644497161d09a83ef456d79f178a10
Gislaine Costa Pereira <62241763+gislainecosta@users.noreply.github.com>
2020-06-02 16:57:57 -0300

    criação da função base

--- semana11/aula 38/src/App.test.js
+++ semana11/aula 38/src/App.test.js
@@ -2,3 +2,19 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
+
+const criarPost = () =>{
+    const utils = render (<App />)
+
+    const input = utils.getByPlaceholderText('Novo post')
+    fireEvent.change(input, {
+        target:{
+            value: "Post Teste"
+        }
+    })
+
+    const button = utils.getByText(/Adicionar/)
+    fireEvent.click(button)
+    return utils
+}
+
