e34c829e2ed40fd080a4e06d6a8299a146108477
Gislaine Costa Pereira <62241763+gislainecosta@users.noreply.github.com>
2020-06-03 17:02:29 -0300

    Continuando estilização

---
+++ semana11/Projeto - Planner/planner-gislaine/src/components/DiaSemana.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App';
+
+const DiaSmena =(props) =>{
+
+    return(
+        <div className="container-dia">
+            <section class="titulo-dia">Domingo</section>
+            <section class="conteudo"></section>
+        </div>
+    )
+}
+
+export default DiaSmena;
