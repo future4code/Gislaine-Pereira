89be22f6d28b0f42ccc8521a2f9dc9af1c2e3611
Gislaine Costa Pereira <62241763+gislainecosta@users.noreply.github.com>
2020-06-03 14:34:03 -0300

    criação da aplicação

---
+++ semana10/labex-gislaine/build/precache-manifest.e38fecf9847c5f6e9c62ff6d53f0aad8.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80213e68798fb0dfabf5b61f0538e548",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9652ccf576591ff4ca08",
+    "url": "/static/css/main.af789495.chunk.css"
+  },
+  {
+    "revision": "2ea974927d7a77f33660",
+    "url": "/static/js/2.da64f660.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.da64f660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9652ccf576591ff4ca08",
+    "url": "/static/js/main.183b9e1d.chunk.js"
+  },
+  {
+    "revision": "f161047e73f1feb2af24",
+    "url": "/static/js/runtime-main.57fd4b26.js"
+  },
+  {
+    "revision": "dc3aa2b66266db650968e1bae94cfd64",
+    "url": "/static/media/Logo2.dc3aa2b6.png"
+  },
+  {
+    "revision": "f8522711c68e6598968a05bcc53f5618",
+    "url": "/static/media/Mercurio.f8522711.jpg"
+  },
+  {
+    "revision": "02a073d4e61708e38c1804057a197a9f",
+    "url": "/static/media/by.02a073d4.png"
+  },
+  {
+    "revision": "e6d3a2c4f26f07fcc5a0e2444b18c899",
+    "url": "/static/media/fundo.e6d3a2c4.jpg"
+  },
+  {
+    "revision": "374feaa9be526292add1dacc0a66bfbd",
+    "url": "/static/media/fundo4.374feaa9.jpg"
+  },
+  {
+    "revision": "a0553f7806afa0c28abd23fdf0c9a80a",
+    "url": "/static/media/ganimedes.a0553f78.jpg"
+  },
+  {
+    "revision": "a7599727b4dba74a9a527642ca2a48a7",
+    "url": "/static/media/jupiter.a7599727.jpg"
+  },
+  {
+    "revision": "fa02896335f873f763d3d9647fdabdcd",
+    "url": "/static/media/marte.fa028963.jpg"
+  },
+  {
+    "revision": "25953a467a54974a0a4c5c5c6c7af5a3",
+    "url": "/static/media/nebula.25953a46.jpg"
+  },
+  {
+    "revision": "5e92d7b3ead977110964ca08b5bd19b7",
+    "url": "/static/media/netuno.5e92d7b3.jpg"
+  },
+  {
+    "revision": "0312576c2035888edb400974578f9598",
+    "url": "/static/media/plutão.0312576c.jpg"
+  },
+  {
+    "revision": "a49713a0d6bffa427cad7f7d49d99d01",
+    "url": "/static/media/saturno.a49713a0.jpg"
+  },
+  {
+    "revision": "17f424a203cb906367ad23c8f25afa7c",
+    "url": "/static/media/tita.17f424a2.jpg"
+  },
+  {
+    "revision": "33ba818fcd43638ec09925d5bef00fea",
+    "url": "/static/media/twt.33ba818f.png"
+  },
+  {
+    "revision": "9c573cec8f9de5d76019d2d6c680130d",
+    "url": "/static/media/urano.9c573cec.jpg"
+  },
+  {
+    "revision": "a732abebd6245f26877f68f52575a525",
+    "url": "/static/media/venus.a732abeb.jpg"
+  }
+]);
