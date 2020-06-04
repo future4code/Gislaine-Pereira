87293ad7615e4677acd6b9329592c680f2613479
Gislaine Costa Pereira <62241763+gislainecosta@users.noreply.github.com>
2020-06-04 03:45:56 -0300

    Estilização

--- semana11/Projeto - Planner/planner-gislaine/src/App.js
+++ semana11/Projeto - Planner/planner-gislaine/src/App.js
@@ -1,95 +1,198 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fade, ThemeProvider, withStyles, makeStyles, createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import './App.css';
-import DiaSemana from './components/DiaSemana'
+import DiaSemana from './components/DiaSemana';
+import TextField from '@material-ui/core/TextField';
+import Fab from '@material-ui/core/Fab';
+import IconeBotao from '@material-ui/icons/LibraryAdd';
+import axios from "axios";
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import IconDate from '@material-ui/icons/Today';
+import FormControl from '@material-ui/core/FormControl';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Button from '@material-ui/core/Button';
 
 const urlBase = "https://us-central1-labenu-apis.cloudfunctions.net/generic/planner-julian-gislaine"
 
+const CampoTexto = withStyles({
+  root: {
+    width: "28vw",
+    '& input:valid + fieldset': {
+      borderColor: '#632D80',
+      borderWidth: 2,
+    },
+    '& input:invalid + fieldset': {
+      borderColor: '#490a58',
+      borderWidth: 2,
+    },
+    '& input:valid:focus + fieldset': {
+      borderLeftWidth: 6,
+      padding: '4px !important', 
+    },
+  },
+})(TextField);
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  extendedIcon: {
+    marginLeft: theme.spacing(1),
+  },
+  extendedIcon2: {
+    marginRight: theme.spacing(1),
+  },
+}));
+
+const MyTheme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#38B8A6",
+      main: "#3f2199",
+      dark: "#490a58",
+      contrastText: "#57FFEA",
+    },
+  }
+})
+
 function App() {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [inputTarefa, setInputTarefa] = useState("")
+  const [selecionaDia, setSelecionaDia] = useState("")
+  const [listaTarefas, setListaTarefas] = useState([])
   
-  const adicionaTarefa = () => {
-    const diaSemana = document.getElementById('selecionar-dia').value
-    const minhaTarefa = document.getElementById('descreve-tarefa')
-    const textoTarefa = document.getElementsByClassName('conteudo')
-    const horarioTarefa = document.getElementById('horario').value
+  useEffect(() => {
+    pegaTarefas()
+  }, [urlBase])
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    if (minhaTarefa.value == '' || minhaTarefa.value == ' ') {
-      alert('Insira pelo menos uma tarefa')
-    } else {
-      switch (diaSemana) {
-        case 'domingo':
-          textoTarefa[0].innerHTML += "<p onclick='riscarTarefa(this)'>" + horarioTarefa + " - " + minhaTarefa.value + '</p>'
-          break;
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-        case 'segunda':
-          textoTarefa[1].innerHTML += "<p onclick='riscarTarefa(this)'>" + horarioTarefa + " - " + minhaTarefa.value + '</p>'
-          break;
-
-        case 'terça':
-          textoTarefa[2].innerHTML += "<p onclick='riscarTarefa(this)'>" + horarioTarefa + " - " + minhaTarefa.value + '</p>'
-          break;
-
-        case 'quarta':
-          textoTarefa[3].innerHTML += "<p onclick='riscarTarefa(this)'>" + horarioTarefa + " - " + minhaTarefa.value + '</p>'
-          break;
-
-        case 'quinta':
-          textoTarefa[4].innerHTML += "<p onclick='riscarTarefa(this)'>" + horarioTarefa + " - " + minhaTarefa.value + '</p>'
-          break;
-
-        case 'sexta':
-          textoTarefa[5].innerHTML += "<p onclick='riscarTarefa(this)'>" + horarioTarefa + " - " + minhaTarefa.value + '</p>'
-          break;
-
-        default:
-          textoTarefa[6].innerHTML += "<p onclick='riscarTarefa(this)'>" + horarioTarefa + " - " + minhaTarefa.value + '</p>'
-          break;
-      }
+  const adicionaTarefa = async () => {
+    setInputTarefa("")
+    setSelecionaDia("")
+    const body ={
+      text: inputTarefa,
+      day: selecionaDia,
     }
-    minhaTarefa.value = ""
+    await axios
+      .post(`${urlBase}`, body)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    pegaTarefas()
   }
 
 
-  const limpezatotal = () => {
-    let tudoEmBranco = document.getElementsByClassName('conteudo')
-
-    for (let i = 0; i < tudoEmBranco.length; i++) {
-      tudoEmBranco[i].innerHTML = " "
-    }
+  const pegaTarefas = async () => {
+    await axios
+    .get(`${urlBase}`)
+    .then(response => {
+      setListaTarefas(response.data)
+    })
+     .catch(err => {
+       console.log(err);
+     });
   }
-
-  const riscarTarefa = (tarefaExecutada) => {
-    tarefaExecutada.style.textDecoration = "line-through";
-  }
-  
   
   return (
     <div id="tela-toda">
-      <header>
+      <MuiThemeProvider theme={MyTheme}>
+        <header>
           <h1>Minha Semana</h1>
-          <textarea wrap="hard" placeholder="Descrição da tarefa" id="descreve-tarefa"></textarea>
-          <select id="selecionar-dia">
-              <option value="domingo">Domingo</option>
-              <option value="segunda">Segunda</option>
-              <option value="terça">Terça</option>
-              <option value="quarta">Quarta</option>
-              <option value="quinta">Quinta</option>
-              <option value="sexta">Sexta</option>
-              <option value="sabado">Sábado</option>
-          </select>
           
-          <button onclick="adicionaTarefa()">Criar Tarefa</button>
-      </header>
-        
-      <div id="tarefas">
-        <DiaSemana className="container-dia"/>
-        <DiaSemana className="container-dia"/>
-        <DiaSemana className="container-dia"/>
-        <DiaSemana className="container-dia"/>
-        <DiaSemana className="container-dia"/>
-        <DiaSemana className="container-dia"/>
-        <DiaSemana className="container-dia"/>
-      </div>
+          <form id="formulario">
+  
+            <CampoTexto
+              className={classes.margin}
+              label="Tarefa"
+              required
+              variant="outlined"
+              value={inputTarefa}
+              onChange={e =>{setInputTarefa(e.target.value)}}
+              id="validation-outlined-input"
+            />
+
+
+            <Fab 
+              onClick={handleClickOpen}
+              variant="extended"
+              size="medium"
+              color="primary"
+            >
+              <IconDate className={classes.extendedIcon2} />
+              {selecionaDia === ""? "Selecione o dia" : selecionaDia}
+            </Fab>
+            
+            <Dialog disableBackdropClick disableEscapeKeyDown open={open} onClose={handleClose}>
+              <DialogContent>
+                <FormControl component="fieldset">
+                  <RadioGroup aria-label="gender" name="gender1" value={selecionaDia} onChange={e =>{setSelecionaDia(e.target.value)}}>
+                    <FormControlLabel value="Domingo" control={<Radio color="primary" />} label="Domingo" />
+                    <FormControlLabel value="Segunda" control={<Radio color="primary" />} label="Segunda" />
+                    <FormControlLabel value="Terça" control={<Radio color="primary" />} label="Terça" />
+                    <FormControlLabel value="Quarta" control={<Radio color="primary" />} label="Quarta" />
+                    <FormControlLabel value="Quinta" control={<Radio color="primary" />} label="Quinta" />
+                    <FormControlLabel value="Sexta" control={<Radio color="primary" />} label="Sexta" />
+                    <FormControlLabel value="Sábado" control={<Radio color="primary" />} label="Sábado" />
+                  </RadioGroup>
+                </FormControl>
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose} color="primary">
+                    Cancela
+                  </Button>
+                  <Button onClick={handleClose} color="primary">
+                    Ok
+                  </Button>
+                </DialogActions>
+            </Dialog>
+  
+            <Fab
+              variant="extended"
+              size="medium"
+              color="primary"
+              aria-label="add"
+              className={classes.margin}
+              onClick={adicionaTarefa}
+            >
+              Criar
+              <IconeBotao className={classes.extendedIcon} />
+            </Fab>
+          </form>
+        </header>
+  
+        <section id="conteudo-principal">
+            <DiaSemana listaTarefasDia={listaTarefas} titulo={"Domingo"}/>
+            <DiaSemana listaTarefasDia={listaTarefas} titulo={"Segunda"}/>
+            <DiaSemana listaTarefasDia={listaTarefas} titulo={"Terça"}/>
+            <DiaSemana listaTarefasDia={listaTarefas} titulo={"Quarta"}/>
+            <DiaSemana listaTarefasDia={listaTarefas} titulo={"Quinta"}/>
+            <DiaSemana listaTarefasDia={listaTarefas} titulo={"Sexta"}/>
+            <DiaSemana listaTarefasDia={listaTarefas} titulo={"Sábado"}/>
+        </section>
+      </MuiThemeProvider>
     </div>
   );
 }
--- semana11/Projeto - Planner/planner-gislaine/src/components/DiaSemana.js
+++ semana11/Projeto - Planner/planner-gislaine/src/components/DiaSemana.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../App';
 
 const DiaSmena =(props) =>{
+    const [tarefasDoDia, setTarefasDoDia] = useState([])
+
+    useEffect(() =>{
+        setTarefasDoDia(props.listaTarefasDia)
+    }, [props.listaTarefasDia])
+
+    const riscarTarefa = (tarefaExecutada) => {
+        tarefaExecutada.style.textDecoration = "line-through";
+    }
+
+    const tarefasNaTela = tarefasDoDia.filter((tarefa, index, array) =>{
+        return tarefa.day === props.titulo
+    }).map((tarefa) => {
+      return <article className="tarefa" id={tarefa.id} key={tarefa.id}>
+          <p> ✔ {tarefa.text}</p>
+      </article>
+    })
 
     return(
-        <div className="container-dia">
-            <section class="titulo-dia">Domingo</section>
-            <section class="conteudo"></section>
+        <div div className = "dia-semana" >
+            <section class="titulo-dia">{props.titulo}</section>
+            <section class="conteudo">{tarefasNaTela}</section>
         </div>
     )
 }
