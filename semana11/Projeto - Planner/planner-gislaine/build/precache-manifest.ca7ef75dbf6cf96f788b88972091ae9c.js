d3b341fe39f7c06707541bd81986fa620ad9ada7
Gislaine Costa Pereira <62241763+gislainecosta@users.noreply.github.com>
2020-06-08 14:07:32 -0300

    criação do App e arquivos de páginas

---
+++ semana11/Projeto - Planner/planner-gislaine/build/precache-manifest.ca7ef75dbf6cf96f788b88972091ae9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f420658336fb18139b1ec81f0d1c8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "463293691a1b43394083",
+    "url": "/static/css/main.2f5ba3e6.chunk.css"
+  },
+  {
+    "revision": "ae46cdccea2f9c0f803b",
+    "url": "/static/js/2.90ab6ddf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.90ab6ddf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "463293691a1b43394083",
+    "url": "/static/js/main.ecc20ffe.chunk.js"
+  },
+  {
+    "revision": "b085d343a5b78de5f8b2",
+    "url": "/static/js/runtime-main.3dd13ffb.js"
+  },
+  {
+    "revision": "824dea9e2a93b64ce878bd0ca1f56f81",
+    "url": "/static/media/error.824dea9e.gif"
+  },
+  {
+    "revision": "31a43c0e7356e8a41ec17bebcaa1099f",
+    "url": "/static/media/loading.31a43c0e.gif"
+  }
+]);
