9de77dbe2009cb5d52e39c2755e78810c70f95dd
Gislaine Costa Pereira <62241763+gislainecosta@users.noreply.github.com>
2020-06-22 18:29:12 -0300

    exercício 3

--- semana14/src/Exercicio3.ts
+++ semana14/src/Exercicio3.ts
@@ -1,1 +1,28 @@
+function verNumeros(sequenciaNumeros: number[]): number | void {
+    
+    type respostas = {
+        quantosNumeros: number,
+        quantosImpares: number,
+        somatorio: number
+    }
 
+    const impares = sequenciaNumeros.filter((numero) =>{
+        return numero % 2 !== 0 
+    });
+
+    let somaTudo: number = 0
+    for (let i = 0; i < sequenciaNumeros.length; i++) {
+        somaTudo += sequenciaNumeros[i]
+    }
+
+
+    const objetoFinal: respostas = {
+        quantosNumeros: sequenciaNumeros.length,
+        quantosImpares: impares.length,
+        somatorio: somaTudo,
+    }
+
+    console.log(objetoFinal)
+}
+
+verNumeros([1,50,35,108,7,86,37])
