bed26d61a71dfc5318c7625103f4388271296102
Gislaine Costa Pereira <62241763+gislainecosta@users.noreply.github.com>
2020-06-23 16:48:57 -0300

    esercícios 1 e 2

---
+++ semana14/Aula 45/src/index.ts
@@ -0,0 +1,46 @@
+
+
+import * as fs from 'fs'
+
+/* EXERCICIO 1 
+
+ A) Acessamos pelos índices da array process.argv[i]
+ 
+ B)
+
+const nome:string = process.argv[2]
+const idade:number = Number(process.argv[3])
+const novaIdade:number = idade + 7
+
+const mensagem: string = `Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${novaIdade}`
+
+console.log(mensagem) */
+
+//===========================================================================================
+
+
+// EXERCICIO 2
+
+function operacaoMatematica(operacao: string, num1: number, num2: number): number {
+  
+  switch (operacao) {
+    case 'sum':
+      return num1 + num2;
+    
+    case 'sub':
+      return num1 - num2;
+    
+    case 'mult':
+      return num1 * num2;
+    
+    default:
+      return num1 / num2;
+  }
+}
+
+const qualoperacao: string = process.argv[2]
+const qualNum1: number = Number(process.argv[3])
+const qualNum2: number = Number(process.argv[4])
+
+console.log(operacaoMatematica(qualoperacao, qualNum1, qualNum2))
+
