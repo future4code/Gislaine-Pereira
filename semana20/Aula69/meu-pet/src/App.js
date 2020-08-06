c27cd913f351c896f41ad8e27b0d1d19de3b9e1b
Gislaine Costa Pereira <62241763+gislainecosta@users.noreply.github.com>
2020-08-06 17:05:59 -0300

    Fazendo o front

--- semana20/Aula69/meu-pet/src/App.js
+++ semana20/Aula69/meu-pet/src/App.js
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import logo from "./img/Logo.png";
 import './App.css';
+import Montagem from './components/Montagem';
+import Icone from '@material-ui/icons/PetsOutlined';
+import { withStyles } from '@material-ui/core/styles';
+import { purple, grey } from '@material-ui/core/colors';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import PetsOutlinedIcon from '@material-ui/icons/PetsOutlined';
+
+const PetIconChecked = withStyles({
+  root: {
+    color: "#3a076b",
+  }
+})(PetsOutlinedIcon);
+
+const PetIcon = withStyles({
+  root: {
+    color: grey[600],
+  }
+})(PetsOutlinedIcon)
+
 
 function App() {
+  const [pet, setPet] = useState("cat")
+
+  const selecionaDog = () =>{
+    setPet("dog")
+  }
+
+  const selecionaCat = () => {
+    setPet("cat")
+  }
+
+  console.log(pet)
+  
   return (
-    <div className="App">
+    <div id="tela-toda">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <div id="conteudo">
+        <section id="enviar-arquivo">
+          <section className="caixa-selecao">
+            <section className="caixa-selecao" onClick={selecionaCat}>
+              {pet === "cat" ? <PetIconChecked /> : <PetIcon />} 
+              <p>Gatíneo</p>
+            </section>
+            
+            <section className="caixa-selecao" onClick={selecionaDog}>
+              {pet === "dog" ? <PetIconChecked /> : <PetIcon />}
+              <p>Doguinho</p>
+            </section>
+            
+          </section>
+        </section>
+  
+        <section id="imagem-pet">
+         
+        </section>
+      </div>
     </div>
   );
 }
---
+++ semana20/Aula69/meu-pet/src/components/Montagem.js
@@ -0,0 +1,13 @@
+import  { React } from 'react';
+
+const Montagem = (props) =>{
+
+    return (
+        <div>
+
+        </div>
+
+    );
+}
+
+export default Montagem;
